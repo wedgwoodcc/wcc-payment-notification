45f4fe1923cae7325eae854031ab21f71b557d51
Chris Haskins <chris@xeelee.org>
2023-10-12 14:33:22 -0700

    Include postgres support for logging requests.

--- api/payment-received.ts
+++ api/payment-received.ts
@@ -1,8 +1,23 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { sql } from '@vercel/postgres';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    const { name = 'World' } = req.query
+export default async function handler(
+    req: VercelRequest, 
+    res: VercelResponse
+) {
+    
+    //const { name = 'World' } = req.query
+
+    try {
+        await sql`insert into paypal_notification(body, received_on) values ('${req.body}', ${new Date(Date.now()).toISOString()});`;
+    } catch (error) {
+        return res.status(500).json({ error })
+    }
+
+    // change this if an error occurs
+    res.status(200)
+
     return res.json({
-        message: `Hello ${name}!!!`,
+        message: `Message received!`,
     })
 }
